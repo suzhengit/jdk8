63a056156ee6dd527fcc7533d264e02e796dd589
苏征 <suzheng@souche.com>
2018-11-29 11:15:17 +0800

    Lambda表达式

--- src/main/java/su/jdk8/code/TimeDemo.java
+++ src/main/java/su/jdk8/code/TimeDemo.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 苏征
@@ -30,17 +32,22 @@ public class TimeDemo {
                 .with(TemporalAdjusters.firstInMonth(DayOfWeek.WEDNESDAY));
         LocalDate date = LocalDate.of(2018, 11, 3);
         DateTimeFormatter formatters = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
-        String text = today.format(formatters);
-        System.out.println(text);
-        Period between = Period.between(date, today);
-        System.out.println("日期相隔" + between.getDays() + "天");
+        DateTimeFormatter localDateFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
+        System.out.println("标准解析日期格式为:" + today.format(localDateFormatter));
+        System.out.println("自定义解析日期格式为:"+ today.format(formatters));
+        Period betweenDay = Period.between(date, today);  //用于计算两个“日期”间隔
+        long differDay = ChronoUnit.DAYS.between(date, today);  //ChronoUnit类可用于在单个时间单位内测量一段时间，例如天数或秒。
+        System.out.println("日期相隔" + betweenDay.getDays() + "天");
+        System.out.println("日期相隔" + differDay + "天");
     }
 
     @Test
     void localTime() {
-        LocalTime time = LocalTime.now();
-        LocalTime time0 = LocalTime.of(15, 43);
-        System.out.println(time);
+        LocalTime start = LocalTime.now();
+        LocalTime end = LocalTime.of(15, 43);
+        System.out.println(start);
+        Duration duration = Duration.between(start, end); //用于计算两个“时间”间隔
+        System.out.println(duration.getSeconds());
     }
 
     @Test
@@ -49,9 +56,19 @@ public class TimeDemo {
         //LocalDateTime转Date
         Date date = Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
         //Date转LocalDateTime
-        LocalDateTime dateTime0 = LocalDateTime.ofInstant(date.toInstant(),ZoneId.systemDefault());
-        //LocalDateTime转字符串
+        LocalDateTime dateTime0 = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        //LocalDateTime自定义格式
         String dateTimeString = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
 
+    @Test
+    void instant()  {
+        Instant start = Instant.now(); //0区时间
+        Instant now = Instant.now().plusMillis(TimeUnit.HOURS.toMillis(8)); //北京时间
+        Date date = new Date();
+        Instant instant = date.toInstant();
+        ZonedDateTime time = start.atZone(ZoneOffset.ofHours(8));
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
     }
 }
