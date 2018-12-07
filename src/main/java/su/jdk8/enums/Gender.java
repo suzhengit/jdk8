aba4fe243a829c8b4cea71a452d5cbcf8b9cb00e
苏征 <suzheng@souche.com>
2018-12-07 13:57:46 +0800

    Lambda表达式

--- src/main/java/su/jdk8/enums/Gender.java
+++ src/main/java/su/jdk8/enums/Gender.java
@@ -1,16 +1,19 @@
 package su.jdk8.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @author 苏征
  * @date 2018-11-27
  */
+@Getter
+@AllArgsConstructor
 public enum Gender {
+
     MALE("男"),
     FEMALE("女");
 
-    String sex;
+   private String sex;
 
-    Gender(String sex) {
-        this.sex = sex;
-    }
 }
